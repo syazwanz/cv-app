4e96d0895cb5e06cf317b2be06e880091545a2e9
syazwanz <syazwanz.dev@gmail.com>
2024-06-23 19:35:29 +0800

    setup api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Options } from '../../types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private httpClient: HttpClient) {}
+
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get(
+      `http://localhost:8080/api${url}`,
+      options
+    ) as Observable<T>;
+  }
+}
---
+++ src/app/services/cv.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CvService {
+  constructor(private apiService: ApiService) {}
+
+  getProfile(id: number): Observable<any> {
+    return this.apiService.get(`/cv/profile/${id}`, {});
+  }
+
+  getSkill(id: number): Observable<any> {
+    return this.apiService.get(`/cv/skill/${id}`, {});
+  }
+
+  getExperience(id: number): Observable<any> {
+    return this.apiService.get(`/cv/experience/${id}`, {});
+  }
+
+  getEducation(id: number): Observable<any> {
+    return this.apiService.get(`/cv/education/${id}`, {});
+  }
+
+  getCourse(id: number): Observable<any> {
+    return this.apiService.get(`/cv/course/${id}`, {});
+  }
+}
