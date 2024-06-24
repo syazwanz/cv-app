498b64df34e5445d8a77a749ba47312c66f347ef
syazwanz <syazwanz.dev@gmail.com>
2024-06-24 15:19:57 +0800

    add page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CvComponent } from './cv/cv.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: CvComponent,
   },
+  { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './page-not-found.component.html',
+})
+export class PageNotFoundComponent {}
